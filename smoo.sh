1c77640541a978551aa6fd0e27d6fcca3e7fbc41
hanbollar <github@hannahbollar.com>
2024-05-21 14:03:44 -0700

    / routing
    Signed-off-by: hanbollar <github@hannahbollar.com>

--- smoo.sh
+++ smoo.sh
@@ -6,9 +6,9 @@ OLDIFS="$IFS"
 IFS=$'\n'
 
 if [ "$LOCAL" == "true" ]; then
-    base_url="http://localhost:8000/v/v0.6.4/"
+    base_url="http://localhost:8000/v/v0.6.4"
 else
-    base_url="https://docs.mrjs.io/v/v0.6.4/"
+    base_url="https://docs.mrjs.io/v/v0.6.4"
 fi
 github_base='https://github.com/Volumetrics-io/documentation/edit/main/source'
 site_name='MRjs'
